56d17b391552d59b25921b0484deeac86a0570e2
NesKitten <nesrineghribi43@gmail.com>
2025-01-13 15:48:40 +0100

    ajout de signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { SelectorComponent } from './selector/selector.component';
 import { BoxesComponent } from './boxes/boxes.component';
 import { BoxService } from './services/box.service';
-import { Box } from './models/box';
 
 @Component({
   selector: 'app-root',
   imports: [BoxesComponent, SelectorComponent],
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
    public title = 'interactive-box-selection';
-   public showSelector!: boolean;
+   public showSelector = signal(false);
   constructor(private boxService: BoxService) {
   }
   ngOnInit(): void {
     this.boxService.idSelectedBox$.subscribe((box: number | null) => {
       if (box !== null) {
-        this.showSelector = true;
+        this.showSelector.set(true);
       } else {
-        this.showSelector = false;
+        this.showSelector.set(false);
       }
     });
   }
